8a0eaea41c796ac5251b254ffb90764bb87a8a21
2156153 <2156153@LLBINFD062218>
2022-05-09 17:58:55 -0400

    Fix URL field validation for follower creation form

--- Models/Follower.cs
+++ Models/Follower.cs
@@ -79,7 +79,7 @@ namespace SessionProject2W5.Models
 		/// serveur grâce au ShortName généré.
 		/// </summary>
 		[MaxLength(100, ErrorMessage = "L'url d'image est trop long! Max. 100 caractères.")]
-		[RegularExpression("^[a-zA-Z0-9./-]*$", ErrorMessage = "L'url contient des caractères invalides!")]
+		[RegularExpression("^[a-zA-Z0-9.:/@#_-]*$", ErrorMessage = "L'url contient des caractères invalides!")]
 		public string ImageUrl { get; set; }
 
 		/// <summary>
